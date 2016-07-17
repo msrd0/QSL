7f8c08515a867600b0916ee5b1e3ede26b33c68d
Dominic <meiserdo@web.de>
2016-07-17 21:00:07 +0200

    update qslvariant

--- core/include/qslvariant.h
+++ core/include/qslvariant.h
@@ -1,29 +1,19 @@
 #pragma once
 
 #include "qsl_global.h"
+#include "qslpassword.h"
 
 #include <QVariant>
 
 namespace qsl {
 
-QVariant qslvariant(const std::string &v)
-{
-	return v.data();
-}
+QVariant qslvariant(const std::string &v);
+QVariant qslvariant(const qsl::Password &p);
 
 // in this case (u)int64_t is a (unsigned) long while qt requires a (unsigned) long long
 #if LONG_MAX > INT_MAX
-
-QVariant qslvariant (int64_t v)
-{
-	return (qlonglong)v;
-}
-
-QVariant qslvariant (uint64_t v)
-{
-	return (qulonglong)v;
-}
-
+QVariant qslvariant (int64_t v);
+QVariant qslvariant (uint64_t v);
 #endif
 
 template<typename T>
