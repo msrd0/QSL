dbce27812d517de784e4b8cd8b1cf355c55187d1
Dominic <meiserdo@web.de>
2016-11-19 14:17:42 +0100

    make sqlite standard driver in examples

--- examples/exampleglobal.h
+++ examples/exampleglobal.h
@@ -3,24 +3,33 @@
 template<typename db_t>
 db_t* connect (int argc, char **argv)
 {
-    db_t *db = 0;
-    if (argc<=1 || strcmp(argv[1], "PostgreSQL")==0)
-    {
-	db = new db_t("psql");
-	db->setHost("localhost");
-	db->setUser("postgres");
-	db->setPassword("postgres");
-    }
-    else
-    {
-	if (strcmp(argv[1], "SQLite")==0)
-	    fprintf(stderr, "Unknown driver %s, falling back to SQLite\n", argv[1]);
-	db = new db_t("sqlite");
-	db->setName("/tmp/qslexample.db");
-    }
-    if (db && db->connect())
-	return db;
-    if (db)
-	delete db;
-    return 0;
+	db_t *db = 0;
+	const char *driver = "sqlite";
+	if (argc > 1)
+		driver = argv[1];
+	if (strcmp(driver, "sqlite") == 0)
+	{
+		db = new db_t(driver);
+		db->setName("/tmp/qslexample.db");
+	}
+	else
+	{
+		db = new db_t(driver);
+		db->setHost("localhost");
+		if (strcmp(driver, "psql") == 0)
+		{
+			db->setUser("postgres");
+			db->setPassword("postgres");
+		}
+		else
+		{
+			db->setUser("root");
+		}
+	}
+	
+	if (db && db->connect())
+		return db;
+	if (db)
+		delete db;
+	return 0;
 }
