7a416cc7cf076ee1ce3d0ee889c2f8b31e0c9f28
Dominic <meiserdo@web.de>
2016-12-09 21:27:52 +0100

    metadata representation for alternate names in db

--- core/spiscolumn.h
+++ core/spiscolumn.h
@@ -5,21 +5,23 @@
 
 #include "spisnamespace.h"
 
+#include <QSet>
 #include <QSharedDataPointer>
 #include <QVariant>
 
 namespace spis {
 
 /**
- * THIS CLASS IS NOT PART OF THE API AND MIGHT CHANGE AT ANY TIME!!!
- * 
  * This class holds all data that a `SPISColumn` stores.
  */
 class SPIS_PRIVATE SPISColumnData : public QSharedData
 {
 public:
+	SPISColumnData() = default;
+	
 	SPISColumnData(const QByteArray &name, const QByteArray &type, int minsize, uint8_t constraints, const QVariant &def)
 		: name(name)
+		, nameInDb(name)
 		, type(type)
 		, minsize(minsize)
 		, constraints(constraints)
@@ -27,12 +29,39 @@ public:
 	{
 	}
 	
-	QByteArray name;
-	QByteArray type;
-	int minsize;
-	uint8_t constraints;
+	SPISColumnData(const QByteArray &name, const QByteArray &nameInDb, const QSet<QByteArray> &alternateNames, bool rename,
+				   const QByteArray &type, int minsize, uint8_t constraints, const QVariant &def)
+		: name(name)
+		, nameInDb(nameInDb)
+		, alternateNames(alternateNames)
+		, rename(rename)
+		, type(type)
+		, minsize(minsize)
+		, constraints(constraints)
+		, def(def)
+	{
+	}
 	
+	/// The name of the column.
+	QByteArray name;
+	/// The name of the column that should be used in the database.
+	QByteArray nameInDb;
+	/// A set of alternative names that may appear in the database.
+	QSet<QByteArray> alternateNames;
+	/// When an alternative name is found in the db and this flag is set to true, the column
+	/// is renamed to `nameInDb`.
+	bool rename = false;
+	
+	/// The SPIS type of the column.
+	QByteArray type;
+	/// The minsize of the column, or -1 to use the largest available.
+	int minsize = -1;
+	/// The constraints of this column.
+	uint8_t constraints = SPIS::none;
+	
+	/// The default value of this column or an invalid or null variant.
 	QVariant def;
+	
 };
 
 /**
@@ -42,15 +71,43 @@ class SPIS_PUBLIC SPISColumn
 {
 public:
 	
-	/// Creates a new column with the given name, type, type's minsize and constraints
-	/// (see `SPIS::ColumnConstraint`).
+	/// Creates a new column with the given name, names to be used in the db including their behaviour,
+	/// type, type's minsize, constraints (see `SPIS::ColumnConstraint`) and default value.
+	/// 
+	/// For a detailed explanation of these parameters take a look at the public fields of `SPISColumnData`.
+	SPISColumn(const QByteArray &name, const QByteArray &nameInDb, const QSet<QByteArray> &alternateNames, bool rename,
+			   const QByteArray &type, int minsize, uint8_t constraints, const QVariant &def)
+		: d(new SPISColumnData(name, nameInDb, alternateNames, rename, type, minsize, constraints, def))
+	{
+	}
+	
+	/// Creates a new column with the given name, type, type's minsize, constraints
+	/// (see `SPIS::ColumnConstraint`) and default value.
+	/// 
+	/// For a detailed explanation of these parameters take a look at the public fields of `SPISColumnData`.
 	SPISColumn(const QByteArray &name, const QByteArray &type, int minsize, uint8_t constraints, const QVariant &def)
 		: d(new SPISColumnData(name, type, minsize, constraints, def))
 	{
 	}
 	
+protected:
+	
+	SPISColumn() : d(new SPISColumnData)
+	{
+	}
+	
+public:
+	
 	/// Returns the name of the column.
 	QByteArray name() const { return d->name; }
+	/// Returns the name of the column that should be used in the database.
+	QByteArray nameInDb() const { return d->nameInDb; }
+	/// Returns the alternative names of the column that can be appear in the database.
+	QSet<QByteArray> alternateNames() const { return d->alternateNames; }
+	/// When an alternative name is found in the db and this method returns true, the column
+	/// should be renamed to `nameInDb`.
+	bool rename() const { return d->rename; }
+	
 	/// Returns the type of the column.
 	QByteArray type() const { return d->type; }
 	/// Returns the type's minsize.
